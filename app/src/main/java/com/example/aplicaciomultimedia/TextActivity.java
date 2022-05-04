51be3ec0ac441e594271d04da51b7d6996340e2e
FrancescBagueMarti <francesc.bague@gmail.com>
2022-05-04 19:21:04 +0200

    Añadido el diseño basico de la TextActivity

--- app/src/main/java/com/example/aplicaciomultimedia/TextActivity.java
+++ app/src/main/java/com/example/aplicaciomultimedia/TextActivity.java
@@ -2,13 +2,50 @@ package com.example.aplicaciomultimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TextActivity extends AppCompatActivity {
 
+    EditText etText;
+    Button btnBuscarArchivo;
+    Button btnGuardar;
+    Button btnCancelar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+
+        initComponents();
+        initEvents();
+    }
+    private void initComponents() {
+        etText = findViewById(R.id.etText);
+        btnBuscarArchivo = findViewById(R.id.btnBuscarArchivo);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        btnCancelar = findViewById(R.id.btnCancelar);
+    }
+    private void initEvents() {
+        btnBuscarArchivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
